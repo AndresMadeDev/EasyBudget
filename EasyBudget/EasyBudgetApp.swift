b6d174e00e5cf5fea528b3276bf767a4e509510b
AndresMadeDev <andres.made.dev@gmail.com>
2023-07-31 20:56:48 -0400

    Initial Commit

---
+++ EasyBudget/EasyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBudgetApp.swift
+//  EasyBudget
+//
+//  Created by Andres Made on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct EasyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
